5617c40a51a764600ba02406b8ec1e63d6e4fb29
liujunyuan <ljyuan@bupt.edu.cn>
2021-04-08 11:42:28 +0800

    encoder

--- transformer.py
+++ transformer.py
@@ -26,7 +26,7 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0,1)
+        pe = pe.unsqueeze(0).transpose(0, 1)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
@@ -35,12 +35,62 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self, Q, K, V, attn_mask):
+        '''
+        Q : [batch_size, n_heads, len_q, d_k]
+        K : [batch_size, n_heads, len_k, d_k]
+        V : [batch_size, n_heads, len_v(=len_k), d_v]
+        attn_mask : [batch_size, n_heads, seq_len, seq_len]
+        '''
+        #  scores : [batch_size, n_heads, len_q, len_k]
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k)
+        scores.masked_fill_(attn_mask, -1e9)
+        #  attn : [batch_size, n_heads, len_q, len_k]
+        attn = nn.Softmax(dim=-1)(scores)
+        #  context : [batch_size, n_heads, len_q, d_v]
+        context = torch.matmul(attn, V)
+        return context, attn
+
 class MultiHeadAttention(nn.Module):
     def __init__(self):
         super(MultiHeadAttention, self).__init__()
         self.W_Q = nn.Linear(d_model, d_k * n_heads, bias=False)
         self.W_K = nn.Linear(d_model, d_k * n_heads, bias=False)
         self.W_V = nn.Linear(d_model, d_v * n_heads, bias=False)
+        self.fc = nn.Linear(n_heads * d_v, d_model, bias=False)
+
+    def forward(self, input_Q, input_K, input_V, attn_mask):
+        '''
+        input_Q : [batch_size, len_q, d_model]
+        input_K : [batch_size, len_k, d_model]
+        input_V : [batch_size, len_v(=len_k), d_model]
+        TODO : len_q len_k len_v = src_len?
+        attn_mask : [batch_size, seq_len, seq_len]
+        '''
+        residual, batch_size = input_Q, input_Q.size(0)
+        #  Q : [batch_size, n_heads, len_q, d_k]
+        Q = self.W_Q(input_Q).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  
+        #  K : [batch_size, n_heads, len_k, d_k]
+        K = self.W_K(input_K).view(batch_size, -1, n_heads, d_k).transpose(1, 2)
+        #  V : [batch_size, n_heads, len_v(=len_k), d_v]
+        V = self.W_V(input_V).view(batch_size, -1, n_heads, d_v).transpose(1, 2)
+
+        #  Q : [batch_size, n_heads, seq_len, seq_len]
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1)
+
+        #  context : [batch_size, n_heads, len_q, d_v]
+        #  attn : [batch_size, n_heads, len_q, len_k]
+        context, attn = ScaledDotProductAttention()(Q, K, V, attn_mask)
+        #  context : [batch_size, len_q, n_heads * d_v]
+        context = context.transpose(1, 2).reshape(batch_size, -1, n_heads * d_v)
+        #  output : [batch_size, len_q, d_model]
+        output = self.fc(context)
+        return nn.LayerNorm(d_model).cuda()(output + residual), attn
+
 
 class PoswiseFeedForwardNet(nn.Module):
     def __init__(self):
@@ -69,6 +119,7 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.pos_emb = PositionalEncoding(d_model)
+
 
 class Transformer(nn.Module):
     def __init__(self):
